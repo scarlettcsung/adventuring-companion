83dfeaf01e30077d80b45bf3c2f7e3e6ae5362d0
Scarlet Sung <85265936+scarlettcsung@users.noreply.github.com>
2026-05-27 04:25:51 +0200

    I forgot about the notebook in the last commit

--- src/ui/components/bottom_tabs.py
+++ src/ui/components/bottom_tabs.py
@@ -12,8 +12,7 @@ def bottom_tabs():
                 .on('click', lambda: ui.navigate.to('/combat'))
             ui.tab('Spellbook', icon='auto_stories').on('click', lambda: ui.navigate.to('/spellbook'))
             ui.tab('Inventory', icon='backpack').on('click', lambda: ui.navigate.to('/inventory'))
-            ui.tab('Notebook', icon='book').classes('text-sky-300') \
-                .on('click', lambda: ui.navigate.to('/notebook'))
+            ui.tab('Notebook', icon='book').on('click', lambda: ui.navigate.to('/notebook'))
             ui.tab('Levelling', icon='table_rows').classes('text-sky-300') \
                 .on('click', lambda: ui.navigate.to('/levelling'))
             ui.tab('Brewery', icon='sports_bar').classes('text-sky-300') \
